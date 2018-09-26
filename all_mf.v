498212c02bd6272dc4341a80c5ddbc2fb219229b
FlorianSteinberg <florian.steinberg@inria.fr>
2018-09-26 09:17:05 +0200

    added sum and func to the all_mf.v file.

--- all_mf.v
+++ all_mf.v
@@ -2,5 +2,7 @@ Require Export mf_set.
 Require Export mf_core.
 Require Export mf_comp.
 Require Export mf_prop.
-Require Export mf_prod.
 Require Export mf_tight.
+Require Export mf_prod.
+Require Export mf_sum.
+Require Export mf_func.
